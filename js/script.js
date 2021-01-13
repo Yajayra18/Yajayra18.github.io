09827d3e9cd7eb5745674357c2023bdfe9bed848
Yajayra <yajayra.arauco@gmail.com>
2021-01-13 18:42:14 -0500

    add ids en resumen,experiencia y proyectos + add proyectos card-proj + js mobile

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var botonDerecho = document.getElementsByClassName('fa-chevron-right');
 var containerCardResumen = document.getElementsByClassName('detail-cards--container');
 var containerCardExperiencia = document.getElementsByClassName('experience-cards--container');
 
-console.log(containerCardExperiencia);
+var containerCardProyecto = document.getElementsByClassName('project-cards--container');
 
 
 /*una vez haga click a la variable botonIzquierdo, este ejecutará la función desplazarIzquierdaResumen*/
@@ -14,6 +14,9 @@ botonDerecho[0].addEventListener("click", desplazarDerechaResumen);
 
 botonIzquierdo[1].addEventListener("click", desplazarIzquierdaExperiencia);
 botonDerecho[1].addEventListener("click", desplazarDerechaExperiencia);
+
+botonIzquierdo[2].addEventListener("click", desplazarIzquierdaProyecto);
+botonDerecho[2].addEventListener("click", desplazarDerechaProyecto);
 
 /*Funciones de scrollhorizontal para el contenedor de las tarjetas(containerCardResumen)*/
 function desplazarIzquierdaResumen(){
@@ -30,4 +33,12 @@ function desplazarIzquierdaExperiencia(){
 
 function desplazarDerechaExperiencia(){
   containerCardExperiencia[0].scrollLeft += 236;
+}
+
+function desplazarIzquierdaProyecto(){
+  containerCardProyecto[0].scrollLeft -= 210;
+}
+
+function desplazarDerechaProyecto(){
+  containerCardProyecto[0].scrollLeft += 210;
 }
