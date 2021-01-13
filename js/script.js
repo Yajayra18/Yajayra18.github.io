a6ebb5e751f088d7677fc3a6e19cf1f5309094a5
Yajayra <yajayra.arauco@gmail.com>
2021-01-12 19:15:41 -0500

    html + css + js main-experience

--- js/script.js
+++ js/script.js
@@ -2,18 +2,32 @@
 Js toma el elemento por el nombre de la clase (ByClassName)*/
 var botonIzquierdo = document.getElementsByClassName('fa-chevron-left');
 var botonDerecho = document.getElementsByClassName('fa-chevron-right');
-var containerCard = document.getElementsByClassName('detail-cards--container');
+var containerCardResumen = document.getElementsByClassName('detail-cards--container');
+var containerCardExperiencia = document.getElementsByClassName('experience-cards--container');
 
-/*una vez haga click a la variable botonIzquierdo, este ejecutará la función desplazarIzquierda*/
-botonIzquierdo[0].addEventListener("click", desplazarIzquierda);
+console.log(containerCardExperiencia);
 
-botonDerecho[0].addEventListener("click", desplazarDerecha);
 
-/*Funciones de scrollhorizontal para el contenedor de las tarjetas(containerCard)*/
-function desplazarIzquierda(){
-  containerCard[0].scrollLeft -= 250;
+/*una vez haga click a la variable botonIzquierdo, este ejecutará la función desplazarIzquierdaResumen*/
+botonIzquierdo[0].addEventListener("click", desplazarIzquierdaResumen);
+botonDerecho[0].addEventListener("click", desplazarDerechaResumen);
+
+botonIzquierdo[1].addEventListener("click", desplazarIzquierdaExperiencia);
+botonDerecho[1].addEventListener("click", desplazarDerechaExperiencia);
+
+/*Funciones de scrollhorizontal para el contenedor de las tarjetas(containerCardResumen)*/
+function desplazarIzquierdaResumen(){
+  containerCardResumen[0].scrollLeft -= 250;
 }
 
-function desplazarDerecha(){
-  containerCard[0].scrollLeft += 250;
+function desplazarDerechaResumen(){
+  containerCardResumen[0].scrollLeft += 250;
+}
+
+function desplazarIzquierdaExperiencia(){
+  containerCardExperiencia[0].scrollLeft -= 231;
+}
+
+function desplazarDerechaExperiencia(){
+  containerCardExperiencia[0].scrollLeft += 231;
 }
